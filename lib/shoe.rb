413c40e67d0acd9525591cf805456dcfb6e6d90e
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 17:52:32 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,13 @@
+class Book
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
